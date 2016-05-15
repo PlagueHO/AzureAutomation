5c0794b9931993e9a6ab9da3c02720795f1968c5
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-05-15 20:48:41 +1200

    Fixed VM Shutdown

--- runbooks/shutdown-allazurevm.ps1
+++ runbooks/shutdown-allazurevm.ps1
@@ -1,17 +1,37 @@
-workflow shutdown-allazurevm
-{
-    $VMs = Get-AzureVM
-    if ($VMs)
-    {
-        foreach ($VM in $VMs)
+workflow shutdown-allazurevm {
+    inlineScript {
+        Import-Module Azure;
+        Import-Module AzureRM.Automation;
+        Import-Module AzureRM.Compute;
+        Import-Module AzureRM.Resources;
+
+        $Credential = Get-AutomationPSCredential `
+            -Name 'AzurePowerShell';
+
+        Login-AzureRmAccount `
+            -Credential $Credential;
+
+        $subs = Get-AzureRmSubscription
+        $VMs = @()
+        foreach ($sub in $subs)
         {
-            "Stopping '$VM.Name'"
-            $null = Stop-AzureVM -Name ($VM.Name)
-            "Stopped '$VM.Name'"
+            "Getting VMs for Subscription '$sub.SubscriptionName'";
+            Select-AzureRmSubscription -SubscriptionName ($sub.SubscriptionName)
+            $VMs += @(Get-AzureRmVM);
         }
-    }
-    else
-    {
-        "No VMs found"
+
+        if ($VMs.Count -gt 0)
+        {
+            foreach ($VM in $VMs)
+            {
+                "Stopping '$VM.Name'";
+                Stop-AzureRmVM -Name ($VM.Name);
+                "Stopped '$VM.Name'";
+            }
+        }
+        else
+        {
+            "No VMs found";
+        }
     }
 }
